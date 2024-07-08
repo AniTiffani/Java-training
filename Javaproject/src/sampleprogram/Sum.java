3933f669d14b8876dc5325f3362add9bbbbbce5d
ANI TIFFANI <ANI TIFFANI@LAPTOP-3VTI97PU>
2024-07-08 12:39:08 +0530

    First commit

---
+++ Javaproject/src/sampleprogram/Sum.java
@@ -0,0 +1,16 @@
+package sampleprogram;
+
+public class Sum {
+	public void num(int n1,int n2)
+	{
+	int sum=n1+n2;
+
+System.out.println("Sum is: "+sum);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Sum obj=new Sum();
+obj.num(20,20);
+	}
+
+}
